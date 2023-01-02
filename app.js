37a7186ea3f77e9fd5bc5e0d071470d9ee6fe9f3
HafeezBaloch997 <hb583449@gmail.com>
2023-01-02 20:48:48 +0500

    Javascript 9 to 11

---
+++ app.js
@@ -0,0 +1,223 @@
+// Chapter 9 to 11
+
+// Qno 1
+
+// var city = prompt("Enter your city name");
+// if(city === "karachi"){
+// document.write("Welcome to city of lights");
+// }
+// else{
+// document.write("not match karchi");
+// }
+
+// Qno 2 
+
+// var name = prompt("Enter your gender");
+// if ( name === "male"){
+//     document.write("Good Morning sir");
+// }
+// else if ( name ==="female"){
+//     document.write("Good Morning Mam"); 
+// }
+// else{
+//     document.write("not bad sir your gender is always good");
+// }
+
+// Qno 3;
+
+// var colour = prompt("Enter colour");
+// if(colour == "red"){
+//     console.log("must stop");
+//     document.write("this condition is red");
+
+// }
+// else if (colour == "yellow"){
+//     console.log("Its ready to move");
+
+// }
+// else if (colour == "green"){
+//     console.log("Move now");
+// }
+
+// else 
+//   console.log("its not traffic signal button");
+
+// document.write("<Table> <th> Signal Colour </th> <th> Message </th> <tr> <td>Red</td>"
+// ,"<td>Must Stop</td> </tr><tr><td>Yellow</td><td>Ready to move</tr>",
+// "<tr> <td>Green</td><td>Move now</td></tr>");
+
+// Qno 4;
+
+// var fuel = prompt("How many liters fuel in your car");
+
+// if( fuel <= "0.25"){
+//     document.write("Please refill the fuel in your car");
+// }
+// else {
+//     document.write(" your fuel average is good");
+// }
+
+// Qno 5;
+
+// var a = 4;
+// if(++a === 5){
+//     alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if(b++ === 5){
+//     alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if(c++ === 13){
+//     alert("condition 1 is true");
+// }
+// if (++c < 14){
+//     alert("condition 3 is true")
+//   }
+//   if(c === 14){
+//     alert("condition 4 is true");
+//     }
+//     var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+//     if("car" < "cat"){
+//         alert("car is smaller than cat");
+//  }
+
+
+// Qno 6;
+
+
+// var sub1 = parseInt(prompt("Enter your sub1"));
+// var sub2 = parseInt(prompt("Enter your sub2"));
+// var sub3 = parseInt(prompt("Enter your sub3"));
+// var totalMarks = parseInt(prompt("Enter all subject total marks"));
+
+//   var obtained_marks = (sub1+sub2+sub3);
+  
+//   var percentage = (obtained_marks / totalMarks) * 100;
+
+//   if(percentage >= 80 ){
+//      document.write("<h1> Marksheet</h1> <h4>Total Marks : "+ totalMarks + "<h4>  Marks Obtained: "
+//      + obtained_marks+ " <h4> And Percentage is: "+ percentage +
+//      "%<h4>Grade : A-one <h4> Remarks: Excellent " );
+//   }
+
+//   else if (percentage >=70){
+    
+//     document.write("<h1> Marksheet</h1> <h4>Total Marks : "+ totalMarks + "<h4>  Marks Obtained: "
+//     + obtained_marks+ " <h4> And Percentage is: "+ percentage +
+//     "%<h4>Grade : A <h4> Remarks: Good" );
+//   }
+
+//   else if (percentage >=60){
+    
+//     document.write("<h1> Marksheet</h1> <h4>Total Marks : "+ totalMarks + "<h4>  Marks Obtained: "
+//     + obtained_marks+ " <h4> And Percentage is: "+ percentage +
+//     "%<h4>Grade : B <h4> Remarks:  You need to improve " );
+//   }
+
+//   else if (percentage < 60){
+//       document.write("<h1> Marksheet</h1> <h4>Total Marks : "+ totalMarks + "<h4>  Marks Obtained: "
+//     + obtained_marks+ " <h4> Percentage is:" + percentage +
+//     "%<h4>Grade: Fail  <h4> Remarks: Sorry " );
+//   }
+//   else
+//     document.write("Not in grade criteria");
+
+
+// // Qno 7;
+// var num1 = parseInt(prompt("Enter a number"));
+// var store = 7;
+
+// if (num1 == store){
+//     document.write("Bingo! Correct Answers");
+// }
+//  else if (num1 ==++store){
+//     document.write("Close enough to the correct answer");    
+// }
+
+// else 
+// document.write("Not a number");
+
+// Qno 8;
+
+
+// var  number= parseInt(prompt("Enter a number"));
+// if (number % 3 === 0 ){
+//     document.write("Its divisble by 3");
+// }
+// else
+// document.write("Its not divisble by 3");
+
+
+// Qno 9
+
+// var num = parseInt(prompt("Enter a number "));
+
+// if (num % 2 === 0){
+//     document.write("its  even number");
+// }
+// else    
+//  document.write("its  odd number");
+
+// Qno 10
+
+// var temp = parseInt(prompt("Enter a temprature"));
+
+// if (temp >=40){
+//     document.write("It is too hot outside.");
+// }
+// else if( temp >=30){
+//     document.write("The Weather today is Normal.");
+// }
+// else if (temp >=20){
+// document.write("Today weather is a cool");
+// }
+// else if (temp >=10){
+//     document.write("OMG! Today’s weather is so Cool.");
+// }
+// else
+//     document.write("Today weather is a very very cool");
+
+// // Qno 11;
+
+// var num1 = parseInt(prompt("Enter a first number"));
+// var num2 = parseInt(prompt("Enter a second number"));
+// var operator =prompt("Enter a first number,+ - * /");
+// var results;
+
+// if (operator === "+"){
+//     results = num1 + num2;
+//     document.write("Your Addition number is a: " + results);
+// }
+
+// else if (operator === "-"){
+//     results = num1 - num2;
+//     document.write("Your Subtraction number is a: " + results);
+// }
+
+// else if (operator === "*"){
+//     results = num1 * num2;
+//     document.write("Your multipication number is a: " + results);
+// }
+
+// else if (operator === "/"){
+//     results = num1 / num2;
+//     document.write("Your division number is a: " + results);
+// }
+// else 
+// alert("you not defined any operators");
+
+
+
